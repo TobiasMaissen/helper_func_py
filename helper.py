1d3090a59cd4945473e014b3a6595b8fbec480c5
TMA_2023 <38552872+TobiasMaissen@users.noreply.github.com>
2023-08-03 17:14:47 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+import tensorflow_hub as hub
 import tensorflow as tf
+import datetime
+
 
 ##############################################################################################################
 def test_helper_file(some_input):
@@ -36,9 +39,16 @@ def plot_loss_curves(history):
 
 ##############################################################################################################
 # create a function to import image and resize it and shape it to the model
-def load_and_prep_image(filename, img_shape=224):
+def load_and_prep_image(filename, img_shape=224, scale=True):
     """
-    Reads an image from filename, turns into tensor, reshape
+    Reads in an image from filename, turns it into a tensor and reshapes into
+    (224, 224, 3)
+
+    Parameters
+    ----------
+    filename (str): string filename of target image
+    img_shape (int): size to resize target image to, default 224
+    scale (bool): whether to scale pixel values to range(0, 1), default True
     """
     # read in the image
     img = tf.io.read_file(filename)
@@ -46,10 +56,12 @@ def load_and_prep_image(filename, img_shape=224):
     img = tf.image.decode_image(img)
     # resize the image
     img = tf.image.resize(img, size=[img_shape, img_shape])
-    # rescale the image
-    img = img / 255.
 
-    return img
+    if scale:
+        # Rescale the image (get all values between 0 and 1)
+        return img/255
+    else:
+        return img
 
 
 ##############################################################################################################
@@ -76,4 +88,124 @@ def pred_and_plot(model, filename, class_names):
     plt.imshow(img)
     plt.title(f"Prediction: {pred_class}")
     plt.axis(False);
-    
+
+
+##############################################################################################################
+def create_model(model_url, num_classes, image_shape):
+  """Takes a TensorFlow Hub URL and creates a Keras Sequential model with it.
+
+  Args:
+    model_url (str): A TensorFlow Hub feature extraction URL.
+    num_classes (int): Number of output neurons in output layer,
+      should be equal to number of target classes, default 10.
+
+  Returns:
+    An uncompiled Keras Sequential model with model_url as feature
+    extractor layer and Dense output layer with num_classes outputs.
+  """
+
+  # Download the pretrained model and save it as a Keras layer
+  feature_extractor_layer = hub.KerasLayer(model_url,
+                                           trainable=False, # freeze the already learned patterns
+                                           name="feature_extraction_layer",
+                                           input_shape=image_shape +(3,))
+
+  # create our own model
+  model = tf.keras.Sequential([
+      feature_extractor_layer,
+      tf.keras.layers.Dense(num_classes, activation="softmax", name="output_layer")
+  ])
+
+  return model
+
+
+##############################################################################################################
+def create_tensorboard_callback(dir_name, experiment_name):
+  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+  print(f"Saving TensorBoard log files to: {log_dir}")
+
+  return tensorboard_callback
+
+
+##############################################################################################################
+# plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+    """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
+    If classes is passed, confusion matrix will be labelled, if not, integer class values
+    will be used.
+
+    Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+
+    Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
+
+    Example usage:
+    make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                            y_pred=y_preds, # predicted labels
+                            classes=class_names, # array of class label names
+                            figsize=(15, 15),
+                            text_size=10)
+    """
+    # Create the confustion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+    n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+    # Plot the figure and make it pretty
+    fig, ax = plt.subplots(figsize=figsize)
+    cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+    fig.colorbar(cax)
+
+    # Are there a list of classes?
+    if classes:
+        labels = classes
+    else:
+        labels = np.arange(cm.shape[0])
+
+    # Label the axes
+    ax.set(title="Confusion Matrix",
+           xlabel="Predicted label",
+           ylabel="True label",
+           xticks=np.arange(n_classes), # create enough axis slots for each class
+           yticks=np.arange(n_classes), 
+           xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+           yticklabels=labels)
+
+    # Make x-axis labels appear on bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+
+    # Set the threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if norm:
+            plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > threshold else "black",
+                     size=text_size)
+        else:
+            plt.text(j, i, f"{cm[i, j]}",
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > threshold else "black",
+                     size=text_size)
+
+    # Save the figure to the current working directory
+    if savefig:
+        fig.savefig("confusion_matrix.png")
